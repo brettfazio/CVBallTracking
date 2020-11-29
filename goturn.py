b7f458db3b21128a64b70f7b5af49bf3958fab3c
William Chen <wchen6255@Knights.ucf.edu>
2020-11-28 22:33:42 -0500

    csrt tracking

--- goturn.py
+++ goturn.py
@@ -8,7 +8,8 @@ if __name__ == '__main__' :
     # Instead of MIL, you can also use
  
     tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
-    tracker_type = tracker_types[2]
+    tracker_type = tracker_types[7]
+    
  
     if tracker_type == 'BOOSTING':
         tracker = cv2.TrackerBoosting_create()
@@ -26,9 +27,10 @@ if __name__ == '__main__' :
         tracker = cv2.TrackerMOSSE_create()
     if tracker_type == "CSRT":
         tracker = cv2.TrackerCSRT_create()
- 
+    
+    print(tracker_type)
     # Read video
-    video = cv2.VideoCapture("sample_data/1LdhIsz6INQ.mp4")
+    video = cv2.VideoCapture("sample_data/cardiac_kemba.mp4")
  
     # Exit if video not opened.
     if not video.isOpened():
@@ -63,7 +65,7 @@ if __name__ == '__main__' :
         ok, bbox = tracker.update(frame)
  
         # Calculate Frames per second (FPS)
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
  
         # Draw bounding box
         if ok:
@@ -76,10 +78,10 @@ if __name__ == '__main__' :
             cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
  
         # Display tracker type on frame
-        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+        cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2)
      
         # Display FPS on frame
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+        cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
  
         # Display result
         cv2.imshow("Tracking", frame)
