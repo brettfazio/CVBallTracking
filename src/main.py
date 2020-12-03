784e67d73ba3a89a096f3d6e384933fe2701e1a3
Brett Fazio <brett.fazio@hotmail.com>
2020-12-03 14:12:46 -0500

    add stubs to main

--- src/main.py
+++ src/main.py
@@ -13,6 +13,19 @@ import sys
 from tracking import opencv_track
 from detect import detect
 
+def yolo_track(video):
+    ok, frame = video.read()
+
+    if not ok:
+        print('Cannot read video file')
+        sys.exit()
+
+def track(video):
+    ok, frame = video.read()
+
+    if not ok:
+        print('Cannot read video file')
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Specify video source
@@ -26,11 +39,8 @@ if __name__ == "__main__":
 
 
     video = cv.VideoCapture(opt.video)
-    ok, frame = video.read()
 
-    if not ok:
-        print ('Cannot read video file')
-        sys.exit()
-
-    
-    bboxes = detect(frame)
+    if opt.mode == 'track':
+        track(video)
+    else:
+        yolo_track(video)
