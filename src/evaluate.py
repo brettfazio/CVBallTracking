d0e9c4fe1b4e0570a68db932dd8b4afe7df26026
William Chen <wchen6255@Knights.ucf.edu>
2020-12-07 20:01:39 -0500

    rename import

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from detect import detect
-from util import compute_iou
+from utility import compute_iou
 
 """
 The following will run YOLO on every frame of the video to use it as a source 
