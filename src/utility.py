05879600ada1dce30a964752342cdf58c6844920
Brett Fazio <brett.fazio@hotmail.com>
2020-12-07 20:20:44 -0500

    add a2d parser to util

--- src/utility.py
+++ src/utility.py
@@ -1,5 +1,9 @@
 import cv2 as cv2
 
+import pandas as pd
+
+PATH_TO_A2D = '../a2d/videoset.csv'
+
 """
 
 Function to be used in multiple parts of our project. It computes the IOU score of two provided bounding boxes.
@@ -50,3 +54,25 @@ def get_tracker(tracker_type):
         tracker = cv2.TrackerCSRT_create()
 
     return tracker
+
+def get_a2d_df():
+    videoset_path = PATH_TO_A2D + 'videoset.csv'
+
+    videoset = pd.read_csv(videoset_path)
+
+    # Ball videos in the dataset will have ID of 34, 35, 36, 39
+    # flying is 34, jumping is 35, rolling is 36, none is 39.
+
+    # First get the names of all the ball videos.
+
+    # Do this so we can more easily index the column
+    videoset['Label_str'] = videoset['Label'].astype(str)
+
+    # Only get ball videos
+    videoset = videoset[videoset.Label_str.str.startswith('3')]
+
+    # At this point all rows are referecning ball videos.
+    # 253 are classified as training samples. 62 are classified as testing samples based on the provided usage.
+
+    train_df = videoset[videoset['Usage'] == 0]
+    test_df = videoset[videoset['Usage'] == 1]
