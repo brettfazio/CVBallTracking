70c61b4695e1462d509c64203b9a6f706738d3a2
Brett Fazio <brett.fazio@hotmail.com>
2020-12-07 21:08:04 -0500

    fix iou

--- src/utility.py
+++ src/utility.py
@@ -12,12 +12,12 @@ Function to be used in multiple parts of our project. It computes the IOU score 
 def compute_iou(bbox_a, bbox_b):
     # find coordinates of intersection
 
-    tl_x = min(bbox_a[0], bbox_b[0])
-    tl_y = min(bbox_a[1], bbox_b[1])
+    tl_x = max(bbox_a[0], bbox_b[0])
+    tl_y = max(bbox_a[1], bbox_b[1])
 
     # since we input as x,y,w,h we need to compute the other coordinate
-    br_x = max(bbox_a[0] + bbox_a[2], bbox_b[0] + bbox_b[2])
-    br_y = max(bbox_a[1] + bbox_a[3], bbox_b[1] + bbox_b[3])
+    br_x = min(bbox_a[0] + bbox_a[2], bbox_b[0] + bbox_b[2])
+    br_y = min(bbox_a[1] + bbox_a[3], bbox_b[1] + bbox_b[3])
 
     # Take the max in case there is no overlap at all (would be negative)
 
@@ -30,6 +30,12 @@ def compute_iou(bbox_a, bbox_b):
 
     a_area = bbox_a[2] * bbox_a[3]
     b_area = bbox_b[2] * bbox_b[3]
+
+    print(inter_area)
+
+    print(a_area)
+
+    print(b_area)
 
     iou = inter_area / float(a_area + b_area - inter_area)
 
