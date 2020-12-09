18371d5659d7b1c8939b81d7e7869762ba6c135d
William Chen <wchen6255@Knights.ucf.edu>
2020-12-09 15:05:12 -0500

    add box comparison for eval

--- src/evaluate.py
+++ src/evaluate.py
@@ -11,7 +11,7 @@ The following will run YOLO on every frame of the video to use it as a source
 of truth to perform IOU. 
 
 This will be useful in the case where CRST is used to track the ball, to see if there
-is any diverage. Possibly if a ball goes through a net or behind a hand, will CRST correct?
+is any divergence. Possibly if a ball goes through a net or behind a hand, will CRST correct?
 """
 def yolo_based_eval(video_file, mapped_predictions):
     # Read video
@@ -27,10 +27,21 @@ def yolo_based_eval(video_file, mapped_predictions):
     ious = []
     iou_counted = 0
 
+    # Get video dimensions
+    frame_width = int(video.get(3)) 
+    frame_height = int(video.get(4)) 
+   
+    size = (frame_width, frame_height) 
+
+    # Initialize video output
+    result = cv2.VideoWriter(f"{video_file}-eval-out.avi",  
+                         cv2.VideoWriter_fourcc(*'XVID'), 
+                         30, size) 
+
     while video.isOpened():
         frame_index += 1
         ok, frame = video.read()
-
+        
         if not ok:
             break
 
@@ -44,15 +55,25 @@ def yolo_based_eval(video_file, mapped_predictions):
         
         if len(new_bboxes) == 0:
             # no detected balls to compare against. Cannot make any assumptions here.
+            predicted_bbox = mapped_predictions[frame_index]
+            if predicted_bbox:
+                p1 = (int(predicted_bbox[0]), int(predicted_bbox[1]))
+                p2 = (int(predicted_bbox[0] + predicted_bbox[2]), int(predicted_bbox[1] + predicted_bbox[3]))
+                cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+                cv2.putText(frame, f"Predicted Box", (0,frame_height-20), cv2.FONT_HERSHEY_SIMPLEX, 0.45,(255,0,0),2)
+                result.write(frame)
             continue
 
         highest = 0
-
+        highest_bbox = None
         # Find the BBox with the highest IOU score, that is the one we wish to compare against
         for bbox in new_bboxes:
             predicted_bbox = mapped_predictions[frame_index]
 
             iou_score = compute_iou(predicted_bbox, bbox)
+            if iou_score > highest:
+                highest = iou_score
+                highest_bbox = bbox
             highest = max(highest, iou_score)
 
         # If no boxes overlap, report that        
@@ -64,9 +85,21 @@ def yolo_based_eval(video_file, mapped_predictions):
         ious.append(highest)
         iou_counted += 1
 
+        p1 = (int(predicted_bbox[0]), int(predicted_bbox[1]))
+        p2 = (int(predicted_bbox[0] + predicted_bbox[2]), int(predicted_bbox[1] + predicted_bbox[3]))
+        cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+
+        p1 = (int(highest_bbox[0]), int(highest_bbox[1]))
+        p2 = (int(highest_bbox[0] + highest_bbox[2]), int(highest_bbox[1] + highest_bbox[3]))
+        cv2.rectangle(frame, p1, p2, (0,255,0), 2, 1)
+
+        cv2.putText(frame, f"Predicted Box", (0,frame_height-20), cv2.FONT_HERSHEY_SIMPLEX, 0.45,(255,0,0),2)
+        cv2.putText(frame, f"Truth Box", (0,frame_height-40), cv2.FONT_HERSHEY_SIMPLEX, 0.45,(0,255,0),2)
+        result.write(frame)
 
     average_iou = sum(ious) / float(iou_counted)
-
+    video.release() 
+    result.release()
     plt.plot(ious)
     plt.show()
 
